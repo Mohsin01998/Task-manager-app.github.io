a15d1e9ddfd626e14b6b15ff56c58200daa334d4
mohsin01998 <mohsin24898@gmail.com>
2023-02-04 04:41:40 +0500

    changes

--- task_managing/settings.py
+++ task_managing/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-r=5*2+es8(8mi2z!93u5qhdar(uxdf_s^$@*&7krw@yn03k2f2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://34.219.72.163:8000/']
+ALLOWED_HOSTS = ['34.219.72.163']
+
 
 # SECURE_SSL_REDIRECT=False
 # SESSION_COOKIE_SECURE=False
